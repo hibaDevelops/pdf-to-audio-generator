6dec8247cde2dd37efc3058913a8fcf2f304a788
hibaDevelops <ullahhibat@gmail.com>
2022-11-01 22:09:52 -0600

    implementing text to voice logic

---
+++ main.py
@@ -0,0 +1,34 @@
+import pyttsx3
+import PyPDF2
+from tkinter.filedialog import *
+
+book = askopenfilename()
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+
+# To access voice property of the speaker:
+voices = speaker.getProperty('voices')
+
+# Set the speaker's gender: 0-> Male (default), 1-> Female
+speaker.setProperty('voice', voices[1].id)
+
+# Iterate through the pages you want to access
+# For accessing specific pages: Iterate through the corresponding page indices
+# Note: Index of first page-> 0
+# Here, entire PDF is accessed:
+for num in range(pages):
+    # To read current page index:
+    page = pdfReader.getPage(num)
+    # To extract the text present in current page:
+    text = page.extractText()
+    # say() function takes a string as the parameter and then queues the same to be converted from text-to-speech
+    speaker.say(text)
+    # runAndWait() function blocks the engine instance until all the currently queued commands are processed
+    speaker.runAndWait()
+
+# To save the audio output as a MP3 file, within this project:
+# Make use of any MP3 player to access this recording whenever required
+# speaker.save_to_file(text, 'audio.mp3')
+# speaker.runAndWait()
